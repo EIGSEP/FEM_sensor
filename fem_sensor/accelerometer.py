fe2c48d2aa634a5401e7f14cc3c512d07b5094ab
Christian Hellum Bye <chbye@berkeley.edu>
2024-07-05 11:15:45 -0700

    split accelerometer.py to module and script

--- fem_sensor/accelerometer.py
+++ fem_sensor/accelerometer.py
@@ -83,19 +83,3 @@ class IMU:
         return theta, phi
 
 
-def main():
-    imu = IMU()
-    print("Press Ctrl+C to stop reading.")
-    try:
-        while True:
-            x, y, z = imu.read_accel()
-            if x is not None:
-                theta, phi = imu.calc_position(x, y, z)
-                print(f"x:{x} y:{y} z:{z} Theta: {theta:.2f} Phi: {phi:.2f}")
-            time.sleep(2)  # 2 second delay.
-    except KeyboardInterrupt:
-        print("Stopped reading position. Self-destruct in 5...4...3...2...1")
-
-
-if __name__ == "__main__":
-    main()
